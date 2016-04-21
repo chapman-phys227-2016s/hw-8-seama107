9237deceb1418ea043453dcf8a1c40a761628e15
seama107 <a8d4c3bab3dd47c89eb25e4486993de9@compute.c.sage-math-inc.internal>
2016-04-21 00:18:30 +0000

    Debugging 1

--- disorder2.py
+++ disorder2.py
@@ -14,13 +14,19 @@ from Particles import Particles
 import sys
 import subprocess
 
+if len(sys.argv) > 1:
+    num_particles = 1
+else:
+    num_particles = sys.argv[1]
+
+
 p = Particles()
 for i in xrange(1, 99):
     for j in xrange(-99,99):
         p.add_particle(i, j)
 
 p.generate_frame()
-for i in range(int(sys.argv[1])):
+for i in range(int(num_particles)):
     for j in range(20):
         p.move()
     p.generate_frame()
